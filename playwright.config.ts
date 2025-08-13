9a688009286cf29ea9addb2ee2ab0594b8b9da89
Suman Dhara <sumandhara341@gmail.com>
2025-08-13 22:29:48 +0530

    this playwright test is running

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  
   workers: process.env.CI ? 1 : 1,
   testMatch: "test.list.ts",
   reporter: [["dot"]], // Correct tuple format
